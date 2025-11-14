477c7675fe6d6a57a213d06cfd2897e7429890b2
ikeda <ikeda@lee-lab.org>
2025-11-14 12:56:56 +0900

    chore: Update version info

--- src/cslrtools2/_version.py
+++ src/cslrtools2/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.1.dev5+ge29f13355.d20251113'
-__version_tuple__ = version_tuple = (0, 0, 1, 'dev5', 'ge29f13355.d20251113')
+__version__ = version = '0.1.dev35+gdf07087c2'
+__version_tuple__ = version_tuple = (0, 1, 'dev35', 'gdf07087c2')
 
 __commit_id__ = commit_id = None
